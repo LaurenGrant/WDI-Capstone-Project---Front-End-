39ce34d3d520fac5964ccfd191b1e2de6f5818a4
Lauren Grant <lgrant1286@gmail.com>
2015-12-14 15:34:10 -0500

    Creates ids and script tags for click handlers.

--- js/ajax.js
+++ js/ajax.js
@@ -17,6 +17,76 @@ var api = {
     });
   },
 
- };
-
 /*---- AJAX ---- */
+
+  register: function register(credentials, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.url + '/register',
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(credentials),
+      dataType: 'json'
+    }, callback);
+  },
+
+  login: function login(credentials, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.url + '/login',
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(credentials),
+      dataType: 'json'
+    }, callback);
+  },
+
+  logout: function logout(id, token, callback) {
+    this.ajax({
+      method: 'DELETE',
+      url: this.url + '/logout/' + id,
+      headers: {
+        Authorization:'Token token=' + token
+      },
+      dataType: 'json'
+    }, callback);
+  },
+
+  listItems: function list(item, token, callback) {
+    this.ajax({
+      method: 'GET',
+      url: this.url + '/items',
+      headers: {
+        Authorization: 'Token token=' + token
+      },
+      dataType: 'json'
+    }, callback);
+  },
+
+  createItem: function create(item, token, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.url + '/items/',
+      headers: {
+        Authorization: 'Token token=' + token
+      },
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(item),
+      dataType: 'json',
+    }, callback);
+  },
+
+
+  editItem: function (item, token, callback) {
+    this.ajax({
+      method: 'PATCH',
+      url: this.url + '/items/' + id,
+      headers: {
+        Authorization: 'Token token=' + token
+      },
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify({}),
+      dataType: 'json'
+    }, callback);
+  },
+
+};
+
---
+++ js/click_handlers.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/*---- Click Handlers ---- */
+
+$('#signin-link').click(function() {
+  $('#hero').hide();
+  $('#loan-items-examples').hide();
+  $('#user-dashboard').show();
+  $('#map').show();
+  // $('#intro-message').hide();
+  // $('#enter-event').hide();
+});
